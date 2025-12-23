4e0cf761268c3c3dcee2b2d29fa6e6f35716216d
tilemo2 <timw3ndel@gmail.com>
2025-12-23 01:49:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const GITHUB_CONFIG = {
     owner: 'tilemo2',      // z.B. 'maxmustermann'
     repo: '19a19639-a95e-41a3-9500-6ceschnubs96e6',             // z.B. 'anniversary-countdown'
     path: 'data.json',                   // Datei wo die Daten gespeichert werden
-    token: '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'          // Personal Access Token (siehe Anleitung)
+    token: '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'          // Personal Access Token (siehe Anleitung)
 };
 
 let anniversaries = [];
